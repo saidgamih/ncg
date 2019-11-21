0d8fd24a9b9b302e1d452a24d840758d5387c868
saidgamih <said.gamih@gmail.com>
2019-11-21 11:34:48 +0100

    First

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.demo').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        fade: true,
+        dots: true,
+        appendDots: $(".slick-dots")
+    });
+
+});
