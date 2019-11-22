ded03a52b944408755297bbdccd6080674023a11
saidgamih <said.gamih@gmail.com>
2019-11-22 18:26:29 +0100

    Third

--- static/js/main.js
+++ static/js/main.js
@@ -7,4 +7,38 @@ $(document).ready(function(){
         appendDots: $(".slick-dots")
     });
 
+    $('.multiple-items').slick({
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 5000,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              autoplay: true,
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+
 });
